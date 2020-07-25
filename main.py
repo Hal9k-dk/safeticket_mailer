a1e8dc20df39951cc85d0b915e6efa4cd60452c4
Dennis Vestergaard Værum <github@varum.dk>
2020-07-26 01:47:34 +0200

    Can look up and print the stats we need, just need to send the emails now

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # ex: set tabstop=8 softtabstop=0 expandtab shiftwidth=2 smarttab:
 
 import unittest
+from io import StringIO
+from csv import DictReader
 
 from lib.safeticket_wrapper import SafeTicket
 
@@ -24,10 +26,32 @@ def main():
 
   tickets = safeticket.get_event_tickets(event_ids[0])
   ticket_ids = [ticket['id'] for ticket in tickets['data']['tickets']]
-  csv = safeticket.export_tickets_stats(event_ids[0], ticket_ids)
+  csv_content = safeticket.export_tickets_stats(event_ids[0], ticket_ids)
 
-  print(csv)
+  _csv_reader = DictReader(
+      StringIO(csv_content),
+      skipinitialspace=True,
+      delimiter=';',
+      quotechar='"')
 
+# pp([row for row in _csv_reader])
+  ticket_types = {}
+  ticket_unions = {}
+
+  for row in _csv_reader:
+    _count = ticket_types.get(row['Billettype'], 0)
+    ticket_types[row['Billettype']] = _count + 1
+
+    for member_tag in ['IDA Medlems ID', 'PROSA medlems ID', 'TL Medlems ID']:
+      if row.get(member_tag, '') != '':
+        _member_ids = ticket_unions.get(row['Billettype'], [])
+        _member_ids.append(row[member_tag])
+        ticket_unions[row['Billettype']] = _member_ids
+
+  print('\n================(Ticket Stats)=================')
+  pp(ticket_types)
+  print('\n============(Ticket and Member IDs)============')
+  pp(ticket_unions)
 
 
 if __name__ == '__main__':
