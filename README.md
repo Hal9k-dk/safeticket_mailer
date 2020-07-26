d1469dd642ac63e02089e102dcfb1286fc7f904e
Dennis Vestergaard Værum <github@varum.dk>
2020-07-26 11:53:13 +0200

    Added README.md and some comment to config.example.py

---
+++ README.md
@@ -0,0 +1,10 @@
+[TOC]
+
+# Intro
+This script/program is made to extract infomation about ticket sells from safeticket.dk and send it in an email to the IT Unions there are specials deal for there menbers.
+
+# Configuration
+Copy the `config.example.py` to `config.py` and change the variables in the file
+
+# A small library have been written to interact with Safeticket
+[safeticket_wrapper](lib/safeticket_wrapper)
--- config.example.py
+++ config.example.py
@@ -1,10 +1,11 @@
 # ex: set tabstop=8 softtabstop=0 expandtab shiftwidth=2 smarttab:
 
 class CONFIG:
-  organization = 'example'
-  username = 'someone'
+  # Login info for Safeticket.dk
+  organization = 'example' # EXAMPLE.safeticket.dk
+  username = 'someone' # Normally, this is an email address
   password = 'secret'
 
   # Used for select the event
-  event_name = 'the name of the event'
+  event_name = 'the name of the event' # Example: "SummerHack 2020"
 
