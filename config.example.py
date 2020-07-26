268a39d0c33fb5cccd848e5a98e6f763b48813a3
Dennis Vestergaard Værum <github@varum.dk>
2020-07-26 17:49:09 +0200

    Added more comment to the config file and it can now send emails

--- config.example.py
+++ config.example.py
@@ -6,6 +6,60 @@ class CONFIG:
   username = 'someone' # Normally, this is an email address
   password = 'secret'
 
-  # Used for select the event
+  # SMTP (mail) server config
+  class SMTP:
+    host = 'example.com'
+    post = 587 # '25, 465, 587'
+    username = 'someone'
+    password = 'secret'
+
+  # List events with the --events argument
+  # Select the event
   event_name = 'the name of the event' # Example: "SummerHack 2020"
+
+  # List fields for all the ticket with the --ticket-fields argument
+  # Select fields with the id of the union member
+  member_fields = ['Union1 Medlems ID', 'Union2 medlems ID', 'Union3 Medlems ID']
+
+  # Create a list of all the unions that needs and email and what it should contain
+  unions = [{
+    # Name of the Union
+    'name': 'Union1',
+    # Subject of The Email
+    'subject': 'Ticket soldt so far',
+    # The of the name of the reciever in the Email Template
+    'to_name':  'Union Person',
+    # The of the email-address of the reciever of the email
+    'to_email': 'Some Persen <sp@union.org>',
+    # The of the name of the sender in the Email Template
+    'from_name':  'Event Persen',
+    # Extra text added efter the name of the sender. E.g. a phone no.
+    'extra_text': 'Phone Nr. 8888 8888 ',
+    # The of the email-address of the sender of the email
+    'from_email': 'Some Event Person <sep@event.com>',
+    # Names of all the tickets types there should be send to the union.
+    # Use the --tickets arguments to list all the tickets type.
+    # You can also use the --ticket-and-member-ids argument to se that
+    # what ticket types there have already been boughtm, based on the  member_fields variable
+    'ticket_type_names': [
+      'Union ticket for kits',
+      'Union ticket for adults'
+    ]
+  }]
+
+  # The template for emails.
+  # The variable {to_name}, {from_name}, {union_name} & {extra_text} all come from
+  # the keys in the above unions variable. You can change the text if you want.
+  email_template = (
+      "Hej {to_name}\n"
+      "\n"
+      "Hermed billetter med tilskud fra {union_name}. "
+      "Skulle der være solgt {union_name}-billetter til personer der viser sig ikke at være medlem, så må I lige sige til.\n"
+      "\n"
+      "-= Billet Info =-\n"
+      "{ticket_info_text}\n"
+      "\n"
+      "Mvh. {from_name}\n"
+      "{extra_text}")
+
 
--- main.py
+++ main.py
@@ -11,34 +11,7 @@ from lib.safeticket_wrapper import SafeTicket
 from config import CONFIG
 
 
-CONFIG.email_template = """Hej {to_name}
 
-Hermed billetter med tilskud fra {union_name}. Skulle der være solgt {union_name}-billetter til personer der viser sig ikke at være medlem, så må I lige sige til.
-
--= Billet Info =-
-{ticket_info_text}
-
-Mvh. {from_name}
-{extra_text}
-"""
-
-CONFIG.member_id_fields = ['IDA Medlems ID', 'PROSA medlems ID', 'TL Medlems ID']
-
-CONFIG.unions = [{
-  'name': 'IDA',
-  'subject': 'Sommerhack solgte billetter indtil videre',
-  'to_name':  'Henriette',
-  'to_email': 'Henriette Rimmer Caspersen <hrc@ida.dk>',
-  'from_name':  'Dennis',
-  'extra_text': 'Tlf. +45 3023 7249',
-  'from_email': 'Dennis Vestergaard Værum <hal9k@varum.dk>',
-  'ticket_type_names': [
-    'IDA medlem: Barn under 12 år - Alle dage (60% rabat)',
-    'IDA medlem: Barn under 12 år - Weekendbillet (60% rabat)',
-    'IDA medlem: Voksen - Alle dage (60% rabat)',
-    'IDA medlem: Voksen - Weekendbillet (60% rabat)'
-  ]
-}]
 
 def args_parser():
   parser = argparse.ArgumentParser(description='SafeTicket Mailer')
@@ -49,12 +22,16 @@ def args_parser():
       default=False, help='Print all events')
   parser.add_argument('--tickets', dest='tickets', action='store_true',
       default=False, help='Print all the types of ticket of an event')
+  parser.add_argument('--ticket-fields', dest='ticket_fields', action='store_true',
+      default=False, help='Print all the possible fields existion for the tickets')
   parser.add_argument('--ticket-stats', dest='ticket_stats', action='store_true',
       default=False, help='Print ticket types and there stats for the event')
   parser.add_argument('--ticket-and-member-ids', dest='ticket_and_member_ids', action='store_true',
       default=False, help='Print ticket and the id of the members there bought them for the event')
   parser.add_argument('--show-emails', dest='show_emails', action='store_true',
       default=False, help='Display the email there is going to be sendt')
+  parser.add_argument('--send-emails', dest='send_emails', action='store_true',
+      default=False, help='This will send the emails to the unions')
 
   # Print --help message if now arguments
   if len(sys.argv)==1:
@@ -110,7 +87,7 @@ def main():
     _count = ticket_types.get(row['Billettype'], 0)
     ticket_types[row['Billettype']] = _count + 1
 
-    for member_tag in CONFIG.member_id_fields:
+    for member_tag in CONFIG.member_fields:
       if row.get(member_tag, '') != '':
         _member_ids = ticket_unions.get(row['Billettype'], [])
         _member_ids.append(row[member_tag])
@@ -119,6 +96,9 @@ def main():
   if args.tickets or args.debug:
     print('\n============(All The Tickets Types)============')
     pp(sorted([ticket['name'] for ticket in tickets['data']['tickets']]))
+  if args.ticket_fields or args.debug:
+    print('\n===(All The Possible Fields of The Tickets)====')
+    pp(sorted(_csv_reader.fieldnames))
   if args.ticket_stats or args.debug:
     print('\n================(Ticket Stats)=================')
     pp(ticket_types)
@@ -134,21 +114,31 @@ def main():
       if union_ids:
         ticket_info_text.append('{}:\n{}\nAntal billet soldt: {}'.format(
             ticket_type_name,
-            '\n'.join([' - {}'.format(ID) for ID in union_ids]),
+            '\n'.join(sorted([' - {}'.format(ID) for ID in union_ids])),
             len(union_ids),
         ))
 
+    msg = CONFIG.email_template.format(
+       to_name=union['to_name'],
+       from_name=union['from_name'],
+       union_name=union['name'],
+       ticket_info_text='\n\n'.join(ticket_info_text),
+       extra_text=union['extra_text'])
+
     if args.show_emails or args.debug:
       print('\n============(Mail - {})============'.format(union['name']))
-      print('TO:   {}'.format(union['to_email']))
-      print('FROM: {}'.format(union['from_email']))
+      print('TO:      {}'.format(union['to_email']))
+      print('FROM:    {}'.format(union['from_email']))
+      print('SUBJECT: {}'.format(union['subject']))
       print('\n---------------------------')
-      print(CONFIG.email_template.format(
-          to_name=union['to_name'],
-          from_name=union['from_name'],
-          union_name=union['name'],
-          ticket_info_text='\n\n'.join(ticket_info_text),
-          extra_text=union['extra_text']))
+      print(msg)
+
+    if args.send_emails:
+      smtpObj = smtplib.SMTP(CONFIG.SMTP.host, SMTP.login.port)
+      smtpObj.login(CONFIG.SMTP.username, SMTP.login.password)
+      smtpObj.sendmail(union[''], union['receivers'], msg)
+      smtp.quit()
+
  
 
 if __name__ == '__main__':
