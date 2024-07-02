22ab7134439cd686001f6272618bb3633ee9b764
Dennis Vestergaard Værum <github@varum.dk>
2024-07-02 12:47:18 +0200

    Improved the function `create_spreadsheet_grouped_by_ticket_type_data` and added the function `create_spreadsheet_grouped_by_buyer_data` to make life easier for people receiving the list of tickets

--- src/lib/misc.py
+++ src/lib/misc.py
@@ -1,15 +1,17 @@
 import argparse
 import mimetypes
+import re
 import smtplib
 import ssl
 import sys
+import io
 from email.header import Header
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import parseaddr
-from io import BytesIO
 from typing import List, Dict, Any, NamedTuple
+
 
 try:
     from odf import opendocument
@@ -18,9 +20,6 @@ try:
 except ModuleNotFoundError:
     print("You need to install odfpy to use this module: python3 -m pip install odfpy")
     sys.exit(1)
-
-
-from config import Union, Config
 
 
 TicketTypesType = Dict[str, List[Dict[str, Any]]]
@@ -72,25 +71,35 @@ class MemoryFile(NamedTuple):
     data: bytes
 
 
-def create_spreadsheet_ticket_data(union: Union, ticket_types: TicketTypesType, config: Config) -> MemoryFile:
+class RowNumber():
+    _number: int
+    def __init__(self):
+        self._number = 0
+
+    def get(self) -> int:
+        self._number += 1
+        return self._number
+
+
+def create_spreadsheet_grouped_by_ticket_type_data(union, ticket_types: TicketTypesType, config) -> MemoryFile:
     _width_in_cm = 2.64
     _number_characters = 12
     _average_character_length_in_cm = _width_in_cm / _number_characters
 
-    columns = config.ticket_fields + union.ticket_fields_extra
+    column_names = config.ticket_fields + union.ticket_fields_extra
 
     doc = opendocument.OpenDocumentSpreadsheet()
     table_all = Table(name='all', stylename="table=all")
 
     union_tickets = sum([ticket for ticket_name, ticket in ticket_types.items()
                          if ticket_name in union.ticket_type_names], [])
-    for index, column in enumerate(columns):
+    for index, column in enumerate(column_names):
         style_name = f"{table_all.getAttribute('stylename')}|column={index+1}"
         table_column = TableColumn(stylename=style_name)
         table_all.addElement(table_column)
 
         calc_length_in_cm = max(
-            [len(union_ticket[columns[index]])
+            [len(union_ticket[column_names[index]])
              for union_ticket in union_tickets
              if union_ticket.get("Ordre") != "manual-ticket"] + [len(column)]
         ) * _average_character_length_in_cm + 0.2
@@ -100,7 +109,7 @@ def create_spreadsheet_ticket_data(union: Union, ticket_types: TicketTypesType, 
         doc.automaticstyles.addElement(style)
 
     for header_index, ticket_type_name in enumerate(union.ticket_type_names):
-        ticket_type = ticket_types[ticket_type_name]
+        tickets = ticket_types[ticket_type_name]
 
         if ticket_type:
             row_ticket_name = TableRow()
@@ -109,7 +118,7 @@ def create_spreadsheet_ticket_data(union: Union, ticket_types: TicketTypesType, 
             cell_ticket_name = TableCell(
                 stylename=style_name,
                 stringvalue=ticket_type_name, valuetype="string",
-                numbercolumnsspanned=columns.__len__(),
+                numbercolumnsspanned=column_names.__len__(),
             )
             style = Style(name=style_name, family=cell_ticket_name.qname[1])
             style.addElement(ParagraphProperties(textalign="center"))
@@ -119,7 +128,7 @@ def create_spreadsheet_ticket_data(union: Union, ticket_types: TicketTypesType, 
             table_all.addElement(row_ticket_name)
 
             row_header = TableRow()
-            for column_index, column in enumerate(columns):
+            for column_index, column in enumerate(column_names):
                 style_name = f"{table_all.getAttribute('stylename')}|column={column_index+1}|header={header_index + 1}"
                 row_header.addElement(
                     TableCell(
@@ -132,28 +141,160 @@ def create_spreadsheet_ticket_data(union: Union, ticket_types: TicketTypesType, 
                 doc.automaticstyles.addElement(style)
             table_all.addElement(row_header)
 
-            for ticket in ticket_type:
-                row_sold_ticket = TableRow()
-                for column in columns:
-                    row_sold_ticket.addElement(
-                        TableCell(
-                            formula='''="{}"'''.format(ticket[column]),
-                        )
-                    )
-                table_all.addElement(row_sold_ticket)
+            for ticket in tickets:
+                ticket_row = TableRow()
+                for header_column in column_names:
+                    value = ticket[header_column]
+                    if re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}$", value):
+                        ticket_row.addElement(TableCell(
+                            datevalue=value, valuetype="date",
+                        ))
+
+                    elif re.search(r"^[0-9]+$", value):
+                        ticket_row.addElement(TableCell(
+                            value=value, valuetype="float",
+                        ))
+
+                    else:
+                        ticket_row.addElement(TableCell(
+                            stringvalue=value, valuetype="string",
+                        ))
+
+                table_all.addElement(ticket_row)
 
             for _ in range(2):
                 table_all.addElement(TableRow())
 
     doc.spreadsheet.addElement(table_all)
 
-    doc.save('/tmp/test.ods')
+    doc.save(f"/tmp/grouped-by-type.{union.name}.ods")
 
-    data = BytesIO()
+    data = io.BytesIO()
     doc.write(data)
     data.seek(0)
 
-    return MemoryFile(filename="tickets-sold.ods", data=data.read())
+    return MemoryFile(filename="tickets-sold_grouped-by-type.ods", data=data.read())
+
+
+def order_ticket_types(ticket: Dict[str, Any]) -> int:
+    value = 0
+    if 'Voksen' in ticket['Billettype']:
+        value += 100
+    if 'Alle dage' in ticket['Billettype']:
+        value += 10
+    return value
+
+def add_cell_format_in_correct_format(row: TableRow, value: str):
+    if re.search(r"^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}$", value):
+        row.addElement(TableCell(
+            datevalue=value, valuetype="date",
+        ))
+
+    elif re.search(r"^[0-9]+$", value):
+        row.addElement(TableCell(
+            value=value, valuetype="float",
+        ))
+
+    else:
+        row.addElement(TableCell(
+            stringvalue=value, valuetype="string",
+        ))
+
+def create_spreadsheet_grouped_by_buyer_data(union, ticket_types: TicketTypesType, config) -> MemoryFile:
+    _width_in_cm = 2.63
+    _number_characters = 12
+    _average_character_length_in_cm = _width_in_cm / _number_characters
+
+    column_names_ticket_fields = ["Billettype"] + config.ticket_fields
+    column_names = column_names_ticket_fields + union.ticket_fields_extra
+
+    doc = opendocument.OpenDocumentSpreadsheet()
+    table_all = Table(name='all', stylename="table=all")
+
+
+    union_tickets = sum([ticket for ticket_name, ticket in ticket_types.items()
+                         if ticket_name in union.ticket_type_names], [])
+    for index, column in enumerate(column_names):
+        style_name = f"{table_all.getAttribute('stylename')}|column={index+1}"
+        table_column = TableColumn(stylename=style_name)
+        table_all.addElement(table_column)
+
+        calc_length_in_cm = max(
+            [len(union_ticket[column_names[index]])
+             for union_ticket in union_tickets
+             if union_ticket.get("Ordre") != "manual-ticket"] + [len(column)]
+        ) * _average_character_length_in_cm + 0.2
+
+        style = Style(name=style_name, family=table_column.qname[1])
+        style.addElement(TableColumnProperties(columnwidth=f"{calc_length_in_cm:0.2f}cm"))
+        doc.automaticstyles.addElement(style)
+
+    group_by_buyer = {}
+    for ticket_type_name in union.ticket_type_names:
+        tickets = ticket_types[ticket_type_name]
+        for ticket in tickets:
+            group_by_buyer.setdefault(ticket["Ordre"], [])
+            group_by_buyer[ticket["Ordre"]].append(ticket)
+    for ordre_index in group_by_buyer:
+        group_by_buyer[ordre_index].sort(key=order_ticket_types, reverse=True)
+
+
+    header_index = 0
+    style_family = "table-cell"
+    style_name = f"{table_all.getAttribute('stylename')}|header={header_index + 1}"
+    style = Style(name=style_name, family=style_family)
+    style.addElement(ParagraphProperties(textalign="center"))
+    doc.automaticstyles.addElement(style)
+
+    row_header = TableRow()
+    for column_index, column in enumerate(column_names):
+        style_name = f"{table_all.getAttribute('stylename')}|column={column_index+1}|header={header_index + 1}"
+        row_header.addElement(
+            TableCell(
+                stylename=style_name,
+                stringvalue=column, valuetype="string",
+            )
+        )
+        style = Style(name=style_name, family=style_family)
+        style.addElement(TableCellProperties(borderbottom="0.74pt groove #000000"))
+        doc.automaticstyles.addElement(style)
+    table_all.addElement(row_header)
+
+    for ticket_order, tickets in group_by_buyer.items():
+        for ticket_index, ticket in enumerate(tickets):
+            if ticket_index == 0:
+                ticket_row = TableRow()
+                for header_column in column_names:
+                    value = ticket[header_column]
+                    add_cell_format_in_correct_format(row=ticket_row, value=value)
+
+                table_all.addElement(ticket_row)
+
+            else:
+                ticket_row = TableRow()
+                for header_column in column_names_ticket_fields:
+                    add_cell_format_in_correct_format(row=ticket_row, value=ticket[header_column])
+                table_all.addElement(ticket_row)
+
+        row_sum = TableRow()
+        row_sum.addElement(TableCell(
+            stylename=style_name,
+            stringvalue=f"Antal billetter købt: {len(tickets)}", valuetype="string",
+        ))
+        table_all.addElement(row_sum)
+
+        for _ in range(1):
+            table_all.addElement(TableRow())
+
+    doc.spreadsheet.addElement(table_all)
+
+    doc.save(f"/tmp/grouped-by-buyer.{union.name}.ods")
+
+    data = io.BytesIO()
+    doc.write(data)
+    data.seek(0)
+
+    return MemoryFile(filename="tickets-sold_grouped-by-buyer.ods", data=data.read())
 
 
 def show_email(msg: str, union: Union, subject: str, cc_emails: List[str],
